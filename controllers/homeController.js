870dc886b56bcec81eda9220a92a5422bdbdbb5b
chehin <chehinjuan@gmail.com>
2021-06-11 20:09:49 -0300

    Fin seccion 32

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,17 @@
-exports.mostrarTrabajos = (req, res) => {
-    res.render('home'), {
-        nombrePagina: 'devJobs',
-        tagline: 'Encuentra y publica Trabajo para devs web',
+const mongoose = require('mongoose');
+const Vacante = mongoose.model('Vacante');
+
+exports.mostrarTrabajos = async (req, res, next) => {
+
+    const vacantes = await Vacante.find();
+
+    if(!vacantes) return next();
+
+    res.render('home', {
+        nombrePagina : 'devJobs',
+        tagline: 'Encuentra y Pública Trabajos para Desarrolladores Web',
         barra: true,
-        boton: true
-    }
+        boton: true,
+        vacantes
+    })
 }
